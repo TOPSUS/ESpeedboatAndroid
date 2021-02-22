20e1db81703b1bf7a3b7719cdf03b43b56d2ff64
sintlort <61153093+sintlort@users.noreply.github.com>
2021-02-22 22:53:46 +0800

    layout main menu

---
+++ app/src/main/java/com/topsus/espeedboat_android/MainMenuActivity.java
@@ -0,0 +1,14 @@
+package com.topsus.espeedboat_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
--- app/src/main/java/com/topsus/espeedboat_android/RegisterActivity.java
+++ app/src/main/java/com/topsus/espeedboat_android/RegisterActivity.java
@@ -2,14 +2,29 @@ package com.topsus.espeedboat_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    Button registerButton;
+    Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         getWindow().setBackgroundDrawableResource(R.drawable.login_bk);
+
+        mContext = this;
+        registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, MainMenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
