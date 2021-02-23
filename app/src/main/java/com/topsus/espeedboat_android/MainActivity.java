cc97d6deabbf095c911838e731cbe1646179ae48
Alsania14 <alingotama14@gmail.com>
2021-02-23 21:44:52 +0800

    Edit Profile
    Menambahkan Edit Profile Activity

--- app/src/main/java/com/topsus/espeedboat_android/MainActivity.java
+++ app/src/main/java/com/topsus/espeedboat_android/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+        Intent intent = new Intent(MainActivity.this, EditProfileActivity.class);
         startActivity(intent);
 
     }
