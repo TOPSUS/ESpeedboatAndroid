dab73fb58b341ffc12595c46a8e3b944e1745d80
Alsania14 <alingotama14@gmail.com>
2021-02-27 22:57:55 +0800

    Perbaikan Profile dan Edit Profile Activity
    Memberikan Cardview pada Edit Profile dan Profile Activity

--- app/src/main/java/com/topsus/espeedboat_android/EditProfileActivity.java
+++ app/src/main/java/com/topsus/espeedboat_android/EditProfileActivity.java
@@ -3,12 +3,25 @@ package com.topsus.espeedboat_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class EditProfileActivity extends AppCompatActivity {
+    private Spinner spJenisKelamin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        /*
+         * BUAT SPINNER
+         */
+        spJenisKelamin = findViewById(R.id.spEditProfileJenisKelamin);
+        String[] items = new String[]{"Laki - Laki", "Perempuan"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_layout, items);
+        spJenisKelamin.setAdapter(adapter);
+        Toast.makeText(this, "SAT", Toast.LENGTH_SHORT).show();
     }
 }
